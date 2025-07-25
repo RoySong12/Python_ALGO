fd7b8e384775b762eeddaf4564c0bce14a36420c
JinHyun Song <010jhs001212@gmail.com>
2025-07-25 18:00:23 +0900

    commit2

--- codeup_100problems.py
+++ codeup_100problems.py
@@ -369,3 +369,118 @@ elif month // 3 == 3:
 else:
     print("winter")
 
+#6077
+n = int(input())
+
+s = 0
+for i in range(1, n + 1):
+    if i % 2 == 0:
+        s = s + i
+
+print(s)
+
+
+#6080
+
+n, m = map(int,(input().split()))
+
+for i in range (1 , n +1):
+    for j in range (1, m+1):
+        print(i, j)
+
+
+
+#6081
+n = int(input(), 16) # 결과는 10진수
+
+# int(문자열, 10) 문자열: 0, 1, 2, ..., 9 => 숫자
+# int(문자열, 16) 문자열: 0, 1, 2, ... , E, F
+
+# for i in range(1,16):
+#     result = n * i
+#     a = format(i, 'X')
+#     b = format(result, 'X')
+#     print(f"{n:X}*{a}={b}")
+n = int(input(), 16)
+
+for i in range(1, 16):
+    print('%X'%n, '*%X'%i, '=%X'%(n*i), sep='')
+
+
+#6082
+n = int(input())
+
+for i in range (1, n+1):
+    if i % 10 == 3:
+        print('X')
+    elif i % 10 == 6:
+        print('X')
+    elif i % 10 == 9:
+        print('X')
+    else:
+        print(i)
+
+
+#6083
+r,g,b = map(int,(input().split()))
+
+
+for i in range (0, r):
+    for j in range(0, g):
+        for k in range(0, b):
+            print(i, j, k)
+print(r*g*b)
+
+
+#6084
+h,b,c,s = map(int,(input().split()))
+
+result = (h * b * c * s)/8/1024/1024
+
+print("%.1f MB" % result)
+
+
+
+#6085
+w, h, b = map(int, input().split())
+
+result = w * h * b / 8 / 1024 / 1024
+
+print("%.2f MB" % result)
+
+
+
+#6086
+n = int(input())
+
+
+sum = 0
+i = 1
+while True:
+    sum = sum + i
+    i = i + 1
+    if sum >= n:
+        break
+print(sum)
+
+
+#6087
+
+n = int(input())
+
+for i in range(1, n+1):
+    if i % 3 == 0:
+        continue
+    print(i,end=' ')
+
+
+#6088
+a, d, n = map(int,input().split())
+
+
+i = 1
+sum = a
+while i < n + 1:
+    print(sum)
+    sum = sum + d
+    i = i + 1
